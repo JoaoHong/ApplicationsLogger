3f2db6d7ece247ff8ae093fff9fdd56fc079c153
João Pedro Macedo Hong <jhong.coderunner@gmail.com>
2024-04-23 18:04:35 -0300

    MongoDB LEitura

---
+++ ApplicationsLogger.Application/Domain/Entities/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationsLogger.Domain.Entities
+{
+    public class Logger
+    {
+        public string NomeAplicacao { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataOperacao { get; set; }
+        public string Usuario { get; set; }
+    }
+}
